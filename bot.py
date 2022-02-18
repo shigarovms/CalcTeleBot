c2bc48f3238416c55d68137fd8f02e5567165fcf
Misha <shigarovms@gmail.com>
2022-02-18 20:03:32 +0300

    14 reqs edited

--- bot.py
+++ bot.py
@@ -1,16 +1,19 @@
 # Telegram bot, that calculates
 
-from time import sleep
+import os
+from boto.s3.connection import S3Connection
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor, exceptions
 from aiogram.types import ContentType, File, Message
-from converter import text_from_ogg
+from asyncio import sleep
 from os import remove
+from converter import text_from_ogg
 from calculations import exp_calculator
-import InlineMarkup as inline_nav
+from InlineMarkup import calcKeyboard
 
-
+# TOKEN = S3Connection(os.environ[''])
+# print(type(TOKEN))
 bot = Bot(token='1110437563:AAHRe3H8X5MUSstLnqy0c1fSyIyCNIBxxmc')
 dp = Dispatcher(bot)
 exp_text = {}
@@ -34,7 +37,7 @@ async def process_start_command(message: types.Message):
     # Приветствуем
     await message.answer('Привет!✋ Я бот-калькулятор. Посчитаю для тебя выражения типа ((34/2-15)**3 или 16*19-177'
                          '\n\nЕще я обрабатываю голосовые сообщения! 144-27 или "5% от 169" Попробуй! ',
-                         reply_markup=inline_nav.calcKeyboard)
+                         reply_markup=calcKeyboard)
 
     await bot.send_message(message.from_user.id, 'Готов считать!')
     await setup_bot_commands(dp)
@@ -48,7 +51,7 @@ async def process_help_command(message: types.Message):
                          'Отправляй его мне без знака = в конце - я посчитаю. '
                          '\n\nЕще я обрабатываю голосовые сообщения! 144-27 или 5% от 169 Попробуй! '
                          '\n\nПример: ((34/2-15)**3',
-                         reply_markup=inline_nav.calcKeyboard)
+                         reply_markup=calcKeyboard)
     await bot.send_message(message.from_user.id, 'Готов считать!')
 
 
@@ -56,7 +59,7 @@ async def process_help_command(message: types.Message):
 @dp.message_handler(regexp=r'\w+\(\)\w*')
 async def quit_protection(message: types.Message):
     await bot.send_message(message.from_user.id, 'Хорошая попытка, мистер хаккер! Но меня так лекго не выключить.',
-                           reply_markup=inline_nav.calcKeyboard)
+                           reply_markup=calcKeyboard)
     await bot.send_message(message.from_user.id, 'Готов считать!')
 
 
@@ -64,24 +67,11 @@ async def quit_protection(message: types.Message):
 @dp.message_handler()
 async def response_message(message: types.Message):
     answer = exp_calculator(message.text)
-    # try:
-    #     x = eval(message.text)
-    #     if x - int(x) == 0:
-    #         answer = int(x)
-    #     else:
-    #         answer = x
-    # except ZeroDivisionError:
-    #     answer = 'делить на ноль нельзя! не я это придумал 😁'
-    # except Exception:
-    #     answer = 'Я умею считать арифметические выражения такие как (34/2-15)**3 или 33%3-7//4'
-    # else:
-    #     answer = f'Готово! Посчитал! Вычислил! Вот, что у меня вышло: {message.text}={answer}'
-    await bot.send_message(message.from_user.id, answer, reply_markup=inline_nav.calcKeyboard)
+    await bot.send_message(message.from_user.id, answer, reply_markup=calcKeyboard)
     await bot.send_message(message.from_user.id, 'Готов считать!')
 
 
-# 1
-# input callback - добавляем символ к выражению в строке ввода
+# 1 input callback - добавляем символ к выражению в строке ввода
 @dp.callback_query_handler(text_contains='input')
 async def expression_consrtuctor_keys(call: types.callback_query):
     symbToAdd = call.data[-1]
@@ -91,8 +81,7 @@ async def expression_consrtuctor_keys(call: types.callback_query):
                                 chat_id=call.from_user.id)
 
 
-# 2
-# delete callback - удаляем последний символ выражения
+# 2 delete callback - удаляем последний символ выражения
 @dp.callback_query_handler(text_contains='delete')
 async def delete_from_expression(call: types.callback_query):
     exp_text[call.from_user.id] = exp_text[call.from_user.id][:-1]
@@ -109,16 +98,16 @@ async def delete_from_expression(call: types.callback_query):
             pass
 
 
-# 3
-# calculate callback - вычисляем выражение
+# 3 calculate callback - вычисляем выражение
 @dp.callback_query_handler(text_contains='calculate')
 async def expression_calculate(call: types.callback_query):
-    if exp_text[call.from_user.id] == '' or exp_text[call.from_user.id] == 'quit()':
+    id = call.from_user.id
+
+    if exp_text[id] == '' or exp_text[id] == 'quit()':
         return
     else:
-
         try:
-            x = eval(exp_text[call.from_user.id])
+            x = eval(exp_text[id])
             if x - int(x) == 0:
                 answer = int(x)
             else:
@@ -126,37 +115,30 @@ async def expression_calculate(call: types.callback_query):
         except ZeroDivisionError:
             answer = 'делить на ноль нельзя! не я это придумал 😁'
         except Exception:
-            await bot.answer_callback_query(callback_query_id=call.id,
-                                            text=f'Ума не приложу, как такое посчитать:'
-                                                 f'\n\n{exp_text[call.from_user.id]}',
-                                            show_alert=True)
-            answer = f'{exp_text[call.from_user.id]}' \
+            await bot.answer_callback_query(callback_query_id=call.id, text=f'Ума не приложу, как такое посчитать:'
+                                                                            f'\n\n{exp_text[id]}', show_alert=True)
+            answer = f'{exp_text[id]}' \
                      f'\n🙄'
-            await bot.edit_message_text(text=answer,
-                                        message_id=(call.message.message_id + 1),
-                                        chat_id=call.from_user.id)
-            sleep(3)
-            await bot.edit_message_text(text=exp_text[call.from_user.id],
-                                        message_id=(call.message.message_id + 1),
-                                        chat_id=call.from_user.id)
+            await bot.edit_message_text(text=answer, message_id=(call.message.message_id + 1), chat_id=id)
+            await sleep(5)
+            await bot.edit_message_text(text=exp_text[id], message_id=(call.message.message_id + 1), chat_id=id)
             return
         else:
-            answer = f'{exp_text[call.from_user.id]}={answer}'
+            answer = f'{exp_text[id]} = {answer}'
             # Обнуляем строку ввода
-            exp_text[call.from_user.id] = ''
-
-    await bot.edit_message_text(text=answer, message_id=(call.message.message_id + 1),
-                                chat_id=call.from_user.id)
+            exp_text[id] = ''
+    await bot.edit_message_text(text=answer, message_id=(call.message.message_id + 1), chat_id=id)
 
 
-# @dp.message_handler(content_types=[ContentType.VOICE])
-# async def voice_message_handler(message: Message):
-#     voice = await message.voice.get_file()
-#     await bot.download_file(file_path=voice.file_path, destination=f'{voice.file_id}.ogg')
-#     expression = text_from_ogg(f'{voice.file_id}.ogg')
-#     remove(f'{voice.file_id}.ogg')
-#     answer = exp_calculator(expression)
-#     await bot.send_message(message.from_user.id, answer)
+# Обработчик голосового сообщения
+@dp.message_handler(content_types=[ContentType.VOICE])
+async def voice_message_handler(message: Message):
+    voice = await message.voice.get_file()
+    await bot.download_file(file_path=voice.file_path, destination=f'{voice.file_id}.ogg')
+    expression = text_from_ogg(f'{voice.file_id}.ogg')
+    remove(f'{voice.file_id}.ogg')
+    answer = exp_calculator(expression)
+    await bot.send_message(message.from_user.id, answer)
 
 
 if __name__ == '__main__':
--- calculations.py
+++ calculations.py
@@ -1,36 +1,48 @@
 
-
+# import re
 from re import match
 
 
+def text_prepared(text):
+    expression = text.lower()
+    expression = expression.replace(' х ', '*')
+    expression = expression.replace(' x ', '*')
+    expression = expression.replace(',', '.')
+    expression = expression.replace('минус', '-')
+
+    expression = expression.replace(' процентов', '%')
+    expression = expression.replace(' процента', '%')
+    expression = expression.replace(' процент', '%')
+    expression = expression.replace('в степени', '**')
+    expression = expression.replace('степени', '**')
+    # print(expression)
+    return expression
+
+
+def procent_calc(exp):
+    procent = float(exp.split('%')[0])
+    tzeloe = float(exp.split(' ')[-1])
+    x = (0.01 * procent * tzeloe)
+    x = int(x) if x - int(x) == 0 else x
+    return x
+
+
 def exp_calculator(text):
-    # Заменим 'x', который выдает гугл на 'умножить' на '*'
-    expression = text.lower().replace('x', '*')
+    expression = text_prepared(text)
 
     # Проверим, подходит ли строка типа "5% от 69"
-    if match(r'\d+%\W\w+\W\d+', text):
-        procent = text.split('%')[0]
-        tzeloe = text.split(' ')[-1]
-        # Подготовим строку для вычисления процента
-        expression = f'0.01*{procent}*{tzeloe}'
-        result = eval(expression)
-        answer = f'------------------>  {text} = {result}  <---------------------'
+    if match(r'\d*.*\d+%\W\w+\W\d+', expression):
+        answer = f'{text} = {procent_calc(expression)}'
         return answer
-
     try:
         x = eval(expression)
-        if x - int(x) == 0:
-            x = int(x)
+        x = int(x) if x - int(x) == 0 else x
     except ZeroDivisionError:
         answer = 'делить на ноль нельзя! не я это придумал 😁'
     except Exception:
-        answer = f'{text} ...хммм\nЯ умею считать арифметические выражения такие как (34/2-15)**3 или 33%3-7//4'
+        answer = f'{expression} ...хммм\nЯ умею считать арифметические выражения такие как (34/2-15)**3 или 33%3-7//4'
     else:
-        answer = f'Готово! Посчитал! Вычислил! Вот, что у меня вышло: {expression}={x}'
+        answer = f'Готово! Вот, что у меня вышло: {expression} = {x}'
 
     return answer
 
-
-# print(exp_calculator('5% от 69'))
-# print(exp_calculator('9 / 10'))
-# print(sympify('9/10'))
